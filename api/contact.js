64b3b937fc2f16f1a783b62cf27ecde0709d3794
Cedric <cedric_vaernewyck@epam.com>
2026-01-02 19:49:11 +0100

    .env

--- api/contact.js
+++ api/contact.js
@@ -1,10 +1,61 @@
-export default function handler(req, res) {
+import formidable from "formidable";
+import nodemailer from "nodemailer";
+
+// Disable default body parsing
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+export default async function handler(req, res) {
     if (req.method === "POST") {
-        console.log("POST /api/contact", req.body);
+        const form = formidable({ multiples: false });
 
-        // SEND TO SMTP
+        form.parse(req, async (err, fields) => {
+            if (err) {
+                console.error("Error parsing form-data:", err);
+                return res.status(500).json({ error: "Form parse error" });
+            }
 
-        res.status(200).json({ message: "Contact received", data: req.body });
+            console.log("Form fields:", fields);
+
+            const { name, company, email, phone, message } = fields;
+
+            const gmail = process.env.GMAIL_USER
+            console.log("gmail user", gmail)
+
+            const pw = process.env.GMAIL_APP_PASSWORD
+            console.log("gmail pw", pw)
+
+            try {
+                const transporter = nodemailer.createTransport({
+                    service: "gmail",
+                    auth: {
+                        user: process.env.GMAIL_USER,
+                        pass: process.env.GMAIL_APP_PASSWORD,
+                    },
+                });
+
+                await transporter.sendMail({
+                    from: process.env.GMAIL_USER,
+                    to: process.env.GMAIL_USER,
+                    subject: `New contact message from ${name}`,
+                    text: `
+Name: ${name}
+Company: ${company}
+Email: ${email}
+Phone: ${phone}
+Message: ${message}
+          `,
+                });
+
+                res.status(200).json({ message: "Message sent successfully!" });
+            } catch (err) {
+                console.error("Error sending email:", err);
+                res.status(500).json({ error: "Failed to send email" });
+            }
+        });
     } else {
         res.status(405).json({ error: "Method not allowed" });
     }
